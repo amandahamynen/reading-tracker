120016248e42c00fc8e8bc164c954b3ea69db03a
Amanda <amanda.hamynen@gmail.com>
2024-09-08 19:16:08 +0300

    frontend gets data from backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,42 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+//import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+// const promise = axios.get('http://localhost:5001/api/books')
+// console.log(promise)
+
+const BookList = ({ books }) => {
+  return (
+    <div className="booklist">
+      {books.map((book) => (
+        <div className="book-view" key={book.id}>
+          <h2>{ book.title }</h2>
+          <p>Written by { book.author }</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const App = () => {
+  const [books, setBooks] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:5001/api/books')
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      console.log(data)
+      setBooks(data)
+    })
+  }, [])
+
+  return (
+    <div>
+      {books && <BookList books={books} />}
+    </div>
+  )
+
+}
 
 export default App
