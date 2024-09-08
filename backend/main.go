72b81115dc5620a833046a95f8921e84fddea05f
Amanda <amanda.hamynen@gmail.com>
2024-09-08 15:32:41 +0300

    backend works with mongoDB

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,161 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Book struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title     string             `json:"title"`
+	Author    string             `json:"author"`
+	Completed bool               `json:"completed"`
+}
+
+var collection *mongo.Collection
 
 func main() {
 	fmt.Println("Hello world")
+
+	if os.Getenv("ENV") != "production" {
+		// Load the .env file if not in production
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file:", err)
+		}
+	}
+
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+
+	defer client.Disconnect(context.Background())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MONGODB ATLAS")
+
+	collection = client.Database("library").Collection("books")
+
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowHeaders: "Origin,Content-Type,Accept",
+	}))
+
+	app.Get("/api/books", getBooks)
+	app.Post("/api/books", addBook)
+	app.Patch("/api/books/:id", updateBook)
+	app.Delete("/api/books/:id", deleteBook)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5001"
+	}
+
+	if os.Getenv("ENV") == "production" {
+		app.Static("/", "./client/dist")
+	}
+
+	log.Fatal((app.Listen("0.0.0.0:" + port)))
+
+}
+
+func getBooks(c *fiber.Ctx) error {
+	var books []Book
+
+	cursor, err := collection.Find(context.Background(), bson.M{})
+
+	if err != nil {
+		return err
+	}
+
+	defer cursor.Close(context.Background())
+
+	for cursor.Next(context.Background()) {
+		var book Book
+		if err := cursor.Decode(&book); err != nil {
+			return err
+		}
+		books = append(books, book)
+	}
+
+	return c.JSON(books)
+}
+
+func addBook(c *fiber.Ctx) error {
+	book := new(Book)
+
+	if err := c.BodyParser(book); err != nil {
+		return err
+	}
+
+	if book.Title == "" {
+		return c.Status(400).JSON(fiber.Map{"error": "Book title cannot be empty"})
+	}
+
+	insertResult, err := collection.InsertOne(context.Background(), book)
+	if err != nil {
+		return err
+	}
+
+	book.ID = insertResult.InsertedID.(primitive.ObjectID)
+
+	return c.Status(201).JSON(book)
+}
+
+func updateBook(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid book ID"})
+	}
+
+	filter := bson.M{"_id": objectID}
+	update := bson.M{"$set": bson.M{"completed": true}}
+
+	_, err = collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"success": true})
+
+}
+
+func deleteBook(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid book ID"})
+	}
+
+	filter := bson.M{"_id": objectID}
+	_, err = collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"success": true})
 }
